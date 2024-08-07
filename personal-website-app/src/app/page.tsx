d53f2e0b8154ae83d8622757002d2867326b7079
Ridhwan Nashir <ridhwanashir@gmail.com>
2024-08-07 12:25:36 +0700

    Add Logo

--- personal-website-app/src/app/page.tsx
+++ personal-website-app/src/app/page.tsx
@@ -1,9 +1,24 @@
 import Image from "next/image";
+import ridhwanlogo from "../../public/ridhwan-logo.png"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div> 
+        <Image
+          src={ridhwanlogo}
+          alt="Logo of Ridhwan Nashir"
+          sizes="50vw"
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+        />
+        <h1 style={{ fontSize: '3rem', marginTop: '20px' }}>
+          Ridhwan Nashir
+        </h1>
+      </div>
+      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -11,7 +26,7 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://google.com"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -107,7 +122,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
