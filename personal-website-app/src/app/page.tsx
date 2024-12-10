d78737b9045ab465a61d7b0ff458abe18fe8108b
Ridhwan Nashir <ridhwanashir@gmail.com>
2024-12-10 14:30:35 +0700

    Works section: add black transparent container

--- personal-website-app/src/app/page.tsx
+++ personal-website-app/src/app/page.tsx
@@ -177,25 +177,38 @@ export default function Home() {
             <div className="absolute inset-0 transition-transform duration-500 ease-in-out transform hover:scale-110">
               <Image src={worksbg_data_ml} alt="background data and ml" className="w-full h-full object-cover" />
             </div>
-            <p className="relative text-2xl text-white z-10">Data & ML</p>
+            {/* Transparent overlay with text */}
+            <div className="relative inset-0 flex items-center justify-center w-full h-16">
+              <div className="absolute inset-0 bg-black opacity-70"></div>
+              <p className="relative text-2xl text-white z-10">Data & ML</p>
+            </div>
           </div>
           <div className="relative flex-1 flex items-center justify-center overflow-hidden">
             <div className="absolute inset-0 transition-transform duration-500 ease-in-out transform hover:scale-110">
               <Image src={worksbg_design} alt="background design" className="w-full h-full object-cover" />
             </div>
-            <p className="relative text-2xl text-white z-10">Designs</p>
+            <div className="relative inset-0 flex items-center justify-center w-full h-16">
+              <div className="absolute inset-0 bg-black opacity-70"></div>
+              <p className="relative text-2xl text-white z-10">Designs</p>
+            </div>
           </div>
           <div className="relative flex-1 flex items-center justify-center overflow-hidden">
             <div className="absolute inset-0 transition-transform duration-500 ease-in-out transform hover:scale-110">
               <Image src={worksbg_software} alt="background software" className="w-full h-full object-cover" />
             </div>
-            <p className="relative text-2xl text-white z-10">Software Engineering</p>
+            <div className="relative inset-0 flex items-center justify-center w-full h-16">
+              <div className="absolute inset-0 bg-black opacity-70"></div>
+              <p className="relative text-2xl text-white z-10">Software Engineering</p>
+            </div>
           </div>
           <div className="relative flex-1 flex items-center justify-center overflow-hidden">
             <div className="absolute inset-0 transition-transform duration-500 ease-in-out transform hover:scale-110">
               <Image src={worksbg_research} alt="background research" className="w-full h-full object-cover" />
             </div>
-            <p className="relative text-2xl text-white z-10">Research and Blogs</p>
+            <div className="relative inset-0 flex items-center justify-center w-full h-16">
+              <div className="absolute inset-0 bg-black opacity-70"></div>
+              <p className="relative text-2xl text-white z-10">Research and Blogs</p>
+            </div>
           </div>
         </div>
       </div>
