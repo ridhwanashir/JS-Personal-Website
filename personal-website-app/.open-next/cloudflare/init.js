52e1c5ecfc62795394dc40b971d56004a3101715
Ridhwan Nashir <ridhwanashir@gmail.com>
2025-10-06 23:45:39 +0700

    fix: Update build and server manifests for consistency and correct file paths

--- personal-website-app/.open-next/cloudflare/init.js
+++ personal-website-app/.open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1759761019057,
+    __BUILD_TIMESTAMP_MS__: 1759761473713,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
